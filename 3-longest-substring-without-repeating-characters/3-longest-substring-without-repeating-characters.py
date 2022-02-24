a4a10882495e8930834aa9f2bc0ea325715f7d7d
Ankit Kumar <49199101+ankitthakur00@users.noreply.github.com>
2022-02-25 00:59:47 +0530

    Time: 97 ms (50.73%), Space: 14.2 MB (58.33%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,11 +3,9 @@ class Solution:
         dic={}
         start=0
         max_len=0
-    
         for i in range(len(s)):
             if s[i] in dic.keys():
                 start=max(start,dic[s[i]]+1)
-        
             dic[s[i]]=i
             max_len=max(i-start+1,max_len)
         return max_len;
