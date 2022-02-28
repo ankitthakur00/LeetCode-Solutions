e7e24825a6d2a95ee205d49192d801877c6b0036
Ankit Kumar <49199101+ankitthakur00@users.noreply.github.com>
2022-02-28 13:03:10 +0530

    Time: 15 ms (89.15%), Space: 13.5 MB (12.80%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def climbStairs(self, n):
-        
-        cs=[]
-        cs.append(1)
-        cs.append(2)
-        for i in range(2,n):
-            cs.append(cs[i-1]+cs[i-2])
-            
-        return cs[n-1]
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
