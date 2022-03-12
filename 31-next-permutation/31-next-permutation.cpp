d2f92ee739391b30673203bbeb42228d90b6c43c
Ankit Kumar <49199101+ankitthakur00@users.noreply.github.com>
2022-03-12 23:35:20 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (75.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void nextPermutation(vector<int> &num) 
-    {
-        if (num.empty()) return;
-        
-        // in reverse order, find the first number which is in increasing trend (we call it violated number here)
-        int i;
-        for (i = num.size()-2; i >= 0; --i)
-        {
-            if (num[i] < num[i+1]) break;
+    void nextPermutation(vector<int>& nums) {
+    	int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
         }
-        
-        // reverse all the numbers after violated number
-        reverse(begin(num)+i+1, end(num));
-        // if violated number not found, because we have reversed the whole array, then we are done!
-        if (i == -1) return;
-        // else binary search find the first number larger than the violated number
-        auto itr = upper_bound(begin(num)+i+1, end(num), num[i]);
-        // swap them, done!
-        swap(num[i], *itr);
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
     }
-};
+}; 
